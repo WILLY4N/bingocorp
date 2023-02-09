2ca8fc43a436cfde2814f8a9e51be2e7f5b01966
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-02-09 11:10:06 -0300

    Commit inicial
    1st commit

---
+++ js/bingo.js
@@ -0,0 +1,2 @@
+if bt e bt ..clicado
+    alert("Bingo");
---
+++ js/marcarCartela.js
@@ -0,0 +1,9 @@
+function marcado(){
+    var botao = document.getElementById("bt01");
+        //botao.disabled = true;
+        botao.style.backgroundColor = "lightgreen";
+
+
+
+
+}
